3262445386585e5cc4a993acfe59a9e718a03b2f
HenryLi-0 <bxsci8266@gmail.com>
2025-08-15 19:36:42 -0400

    some collision boxes and testing mechanism movement stuff

--- vectors/scenes/robots/1778/commands.gd
+++ vectors/scenes/robots/1778/commands.gd
@@ -1,9 +1,20 @@
 extends Node
 
 @onready var end_back_rollers: RigidBody3D = $"../ElevatorStageOne/ElevatorStageTwo/Arm/EndBackRollers"
-@onready var end_front_roller: RigidBody3D = $"../ElevatorStageOne/ElevatorStageTwo/Arm/EndFrontRoller"
+@onready var end_front_rollers: RigidBody3D = $"../ElevatorStageOne/ElevatorStageTwo/Arm/EndFrontRollers"
+@onready var arm: RigidBody3D = $"../ElevatorStageOne/ElevatorStageTwo/Arm"
+const GOAL:int = 1
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	#print(arm.rotation_degrees.z)
+	print(end_front_rollers.angular_velocity.x)
 	if Input.is_action_pressed("test"):
-		end_front_roller.add_constant_torque(Vector3(-1 * delta,0,0))
+		arm.constant_torque = Vector3(0,0,(-PI/6 - arm.rotation.z) * delta * 0.2)
+		end_front_rollers.add_constant_torque(Vector3(-1 * delta,0,0))
 		end_back_rollers.add_constant_torque(Vector3( 1 * delta,0,0))
+	else:
+		arm.constant_torque = Vector3.ZERO
+		end_front_rollers.constant_torque = Vector3.ZERO
+		end_back_rollers.constant_torque = Vector3.ZERO
+		end_front_rollers.angular_velocity *= 0.7
+		end_back_rollers.angular_velocity *= 0.7
