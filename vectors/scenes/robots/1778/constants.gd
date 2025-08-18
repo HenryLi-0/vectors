c90dc708c18f73009a73dd504a1cb3712b0800a4
HenryLi-0 <bxsci8266@gmail.com>
2025-08-18 00:45:58 -0400

    08172025 - more code, coral, and added ground intake collision box!

--- vectors/scenes/robots/1778/constants.gd
+++ vectors/scenes/robots/1778/constants.gd
@@ -4,5 +4,5 @@ extends Node
 static var DRIVE_MAX_SPEED:float = 4 # m/s
 static var DRIVE_MAX_ACCEL:float = 6 # m/s^2
 static var DRIVE_MAX_ROT:float = 1.8 * PI # radians?
-static var DRIVE_FORCE: float = 30000 # ???
-static var TURN_FORCE: float = 200
+static var DRIVE_FORCE: float = 120000 # ???
+static var TURN_FORCE: float = 800
