5423991c01a632330788aa24b1c170344b8c10ab
HenryLi-0 <bxsci8266@gmail.com>
2025-09-01 23:20:28 -0400

    safety commit

--- vectors/scenes/field/s_reefBranch.gd
+++ vectors/scenes/field/s_reefBranch.gd
@@ -1,8 +1,14 @@
 extends Area3D
 
 '''The number of points a branch is worth!'''
-@export var points:int = 0
+@export var autoPoints:int = 0
+@export var telePoints:int = 0
 
-'''This class shouldn't do the point scoring, the coral should!'''
 func _ready() -> void:
 	name = "REEF_BRANCH"
+
+func _on_body_entered(body: Node3D) -> void:
+	print(body.name + ": found something")
+	if body is RigidBody3D:
+		if body.name.begins_with("Coral"):
+			print("kachow")
