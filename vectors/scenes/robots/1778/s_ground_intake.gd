3e84a452f01b1909bd87ad7d627e6f4d698774d4
HenryLi-0 <bxsci8266@gmail.com>
2025-09-19 00:58:43 -0400

    09182025 - reef poles added to main field, pid into its own file, model updates

--- vectors/scenes/robots/1778/s_ground_intake.gd
+++ vectors/scenes/robots/1778/s_ground_intake.gd
@@ -6,31 +6,6 @@ extends RigidBody3D
 
 @onready var detection: Area3D = $detection
 
-class PIDController:
-	var p: float
-	var i: float
-	var d: float
-	var lastMeasurement: float
-	var lastError: float
-	var totalError: float
-	
-	func _init(p:float, i:float, d:float, measurement:float = 0) -> void:
-		self.p = p
-		self.i = i
-		self.d = d
-		self.lastMeasurement = measurement
-		self.lastError = 0
-		self.totalError = 0
-	func calculate(measurement:float, goal:float, deltaTime:float) -> float:
-		self.totalError += (goal-measurement)*deltaTime
-		var value = (self.p*(goal-measurement)
-			+ self.i*(self.totalError)
-			+ self.d*((goal-measurement)-self.lastError)/(deltaTime+1e-6)
-		)
-		self.lastMeasurement = measurement
-		self.lastError = goal-measurement
-		return value
-
 var pidController:PIDController = PIDController.new(kP, kI, kD)
 var goal:float = 0
 var pos:float = 0
